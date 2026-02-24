d7258fc827ce8388323664e4f58da7ed9729dc96
Atilla Akkaya <118857762+dutpekmezi@users.noreply.github.com>
2026-02-24 16:03:43 +0300

    Add Gul-inspired global storefront CSS theme

--- app/root.tsx
+++ app/root.tsx
@@ -8,9 +8,12 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import "./app.css";
+import appStylesHref from "./app.css?url";
+import gulStylesHref from "./styles/gul.css?url";
 
 export const links: Route.LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+  { rel: "stylesheet", href: gulStylesHref },
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
@@ -19,7 +22,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Assistant:wght@200..800&display=swap",
   },
 ];
 
